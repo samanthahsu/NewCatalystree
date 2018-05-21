81404cdc5eab4b130899e4dae18340affd423d56
samanthahsu <sam06207891@gmail.com>
2018-05-21 09:31:24 -0700

    Update MainActivity.java

--- Catalystree/java/MainActivity.java
+++ Catalystree/java/MainActivity.java
@@ -1,30 +1,16 @@
 package com.example.newcatalystree;
 
-import android.content.Intent;
 import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+public class MainActivity extends AppCompatActivity implements SignUpFragment.OnFragmentInteractionListener {
 
-public class MainActivity extends AppCompatActivity implements SignUp.OnFragmentInteractionListener {
-
-    private Button btLogIn;
-    private Fragment mLogIn;
     FrameLayout fragContainer;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,24 +28,25 @@ public class MainActivity extends AppCompatActivity implements SignUp.OnFragment
             }
         });
 
-
-/*
 //        login button opens login fragment when clicked
-        btLogIn = findViewById(R.id.btLogIn);
-        btSignUp.setOnClickListener(new View.OnClickListener() {
+        Button btLogIn = findViewById(R.id.btLogInMain);
+        btLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                mLogIn = new LogIn();
-                transaction.replace(R.id.flFragContainer, mLogIn);
-                transaction.commit();
+                openLogInFragment();
             }
-        }); // todo: create proper login fragment after making sure the signup works
-*/
+        });
     }
 
     private void openSignUpFragment() {
-        SignUp fragment = SignUp.newInstance(); // calling method inside fragment
+        SignUpFragment fragment = SignUpFragment.newInstance(); // calling method inside fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.addToBackStack(null); // allows < to close only this frag not whole activity
+        transaction.replace(R.id.flFragContainer, fragment).commit(); //.commit() executes line
+    }
+
+    private void openLogInFragment() {
+        LogInFragment fragment = LogInFragment.newInstance(); // calling method inside fragment
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.addToBackStack(null); // allows < to close only this frag not whole activity
         transaction.replace(R.id.flFragContainer, fragment).commit(); //.commit() executes line
