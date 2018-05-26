9b9c0b295e69c4fc2b88c2347e58154b2165956d
samanthahsu <sam06207891@gmail.com>
2018-05-26 11:32:19 -0700

    Create ArchiveFragment.java

---
+++ Catalystree/java/ArchiveFragment.java
@@ -0,0 +1,84 @@
+package com.example.newcatalystree;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ArchiveFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ArchiveFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ArchiveFragment extends Fragment {
+    // : Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // todo make array that stores image thumb, string title, string description
+    // todo use the array to inflate
+
+    private OnFragmentInteractionListener mListener;
+
+    public ArchiveFragment() {
+        // Required empty public constructor
+    }
+
+
+    //    create new instance of this fragment
+    public static ArchiveFragment newInstance() {
+        ArchiveFragment fragment = new ArchiveFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+//        todo make grid view
+        return inflater.inflate(R.layout.fragment_archive, container, false);
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
