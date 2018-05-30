c86b4dbd9cd57f2ea11829ad1b73f0419d5f3532
samanthahsu <sam06207891@gmail.com>
2018-05-30 12:21:51 -0700

    Update GlobalActivity.java

--- Catalystree/java/GlobalActivity.java
+++ Catalystree/java/GlobalActivity.java
@@ -3,6 +3,7 @@ package com.example.newcatalystree;
 import android.app.Application;
 
 import com.firebase.client.Firebase;
+import com.google.firebase.database.FirebaseDatabase;
 
 // class added to manifest
 
@@ -14,5 +15,6 @@ public class GlobalActivity extends Application {
 
 //        spreads firebase throughout app so we dont have to write this line in every class
         Firebase.setAndroidContext(this);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true); // store offline data
     }
 }
