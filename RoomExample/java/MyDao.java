dbf9fa799110637f4871d724d4d9de28bc4b23ad
samanthahsu <sam06207891@gmail.com>
2018-06-11 16:12:22 -0700

    Create MyDao.java

---
+++ RoomExample/java/MyDao.java
@@ -0,0 +1,28 @@
+package com.example.roomexample;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface MyDao {
+
+    @Insert
+    public void addUser(User user);
+
+
+    @Query("select * from users") // users is table name
+    public List<User> getUsers();
+
+    @Delete
+    public void deleteUser(User user);
+
+    @Update
+    public void updateUser(User user);
+
+
+}
